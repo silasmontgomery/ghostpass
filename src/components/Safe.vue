ed489895625ef1988bdb3dbbfd97bd6acda8e43a
Silas Montgomery <silas@slimtechnologies.com>
2019-12-17 10:05:29 -0500

    Basic optimizations

--- src/components/Safe.vue
+++ src/components/Safe.vue
@@ -36,10 +36,10 @@
       <h2>Enter a passphrase to decrypt this Ghostpass safe.</h2>
       <form @submit.prevent>
         <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="passphrase">
             Passphrase
           </label>
-          <input :class="error.passphrase ? 'border-red-500':''" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" v-model="passphrase" type="password" placeholder="******************" autocomplete="new-password" />
+          <input :class="error.passphrase ? 'border-red-500':''" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" v-model="passphrase" id="passphrase" type="password" placeholder="******************" autocomplete="new-password" />
           <p v-if="error.passphrase" class="text-red-500 text-xs italic">{{ error.passphrase }}</p>
         </div>
         <button :disabled="!passphrase || passphrase.length < 6" class="btn btn-blue" @click="decryptSafe">Decrypt and open safe</button>
