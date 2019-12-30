bb461335dbb72df63fe70a5a68a0cdad9cb77a86
Silas Montgomery <silas@slimtechnologies.com>
2019-12-29 23:06:36 -0500

    Working on workbox caching

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,12 @@ module.exports = {
     runtimeCompiler: true,
     devServer: {
         before: configureAPI
+    },
+    pwa: {
+        name: 'Ghostpass',
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            swSrc: 'src/service-worker.js'
+        }
     }
 }
